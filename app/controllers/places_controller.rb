359ac70a4da4204ba2247806ce5a525d17169a6a
MaAnhQuan <ma.quan.1711@gmail.com>
2017-10-08 20:46:41 +0700

    Add and delete place.

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,52 @@
+class PlacesController < ApplicationController
+  before_action :find_place, only: %i{show destroy}
+  before_action :admin, except: :index
+  def index
+    @places = Place.paginate page: params[:page]
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new params.require(:place).permit(:name, :address, :overview)
+    if @place.save
+      flash[:success] = "Place created."
+      redirect_to places_url
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @place.destroy
+    flash[:success] = "Place deleted success."
+    redirect_to places_url
+  end
+
+  private
+  def find_place
+    @place = Place.find_by(id: params[:id])
+
+    return if @places
+    flash[:danger] = "Place not exist!"
+    #redirect_to root_url #cho ve trang chu, may muon lam the nao cung dc
+  end
+
+  def admin
+    return if current_user.admin
+
+    flash[:danger] = "You are not admin."
+    redirect_to places_url
+  end
+end
