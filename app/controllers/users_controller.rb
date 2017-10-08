b0517bb9c6def51fe7336d37081327398efa037e
Phan Anh Trung <phananhtrung.140895@gmail.com>
2017-10-08 02:19:41 +0000

    demo fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
     def show
-        @user = !params[:id].nil? ? User.find(params[:id]) : current_user
+        @user = !params[:id].nil? ? User.find_by(id: params[:id]) : current_user
     end
 end
