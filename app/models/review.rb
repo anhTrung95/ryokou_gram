95be376a5e1c9dec8eb30190e4a33bedbde81327
To Anh Dung <toanhdung13041995@gmail.com>
2017-10-15 17:57:40 +0000

    tagging

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
-
+  acts_as_taggable_on :tags
   scope :order_desc, ->{order created_at: :desc}
 
   validates :user_id, presence: true
