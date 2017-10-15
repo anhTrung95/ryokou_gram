be729358457bee5b771f0dd089b463046d914479
Phan Anh Trung <phananhtrung.140895@gmail.com>
2017-10-15 07:36:13 +0000

    fix place_id cua review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,6 +24,6 @@ class ReviewsController < ApplicationController
   private
 
     def review_params
-      params.require(:review).permit(:rate, :content)
+      params.require(:review).permit(:rate, :content, :place_id)
     end
 end
