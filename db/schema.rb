aa3aceb15500c8070dcdd92484b2812007883927
Vu Anh Son <vuanhsonbkhn@gmail.com>
2017-11-19 06:02:23 +0000

    Completed login with facebook

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111031523) do
+ActiveRecord::Schema.define(version: 20171119055809) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -89,8 +89,30 @@ ActiveRecord::Schema.define(version: 20171111031523) do
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.boolean  "gender"
+    t.string   "phone"
+    t.boolean  "admin"
+    t.string   "avatar"
+    t.string   "uid"
+    t.string   "provider"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
